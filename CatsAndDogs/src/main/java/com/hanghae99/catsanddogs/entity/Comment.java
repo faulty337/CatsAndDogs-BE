9eef15ba650b295d74a539d17136e52372fe5628
sooni2 <jinong22@gmail.com>
2022-12-16 20:46:45 +0900

    feat - Entity 생성 시도

--- CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/entity/Comment.java
+++ CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/entity/Comment.java
@@ -1,4 +1,20 @@
 package com.hanghae99.catsanddogs.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PostId")
+    private Post post;
 }
