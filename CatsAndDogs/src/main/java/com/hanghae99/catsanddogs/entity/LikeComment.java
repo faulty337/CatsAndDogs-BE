e8d28a03a883fa0c56151f9736f58960d85656f2
sooni2 <jinong22@gmail.com>
2022-12-17 20:28:28 +0900

    add - Like 관련 Entity 수정

--- CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/entity/LikeComment.java
+++ CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/entity/LikeComment.java
@@ -2,6 +2,7 @@ package com.hanghae99.catsanddogs.entity;
 
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
+@NoArgsConstructor
 @Entity
 public class LikeComment {
 
@@ -20,6 +22,11 @@ public class LikeComment {
     private Long commentId;
 
     private Long userId;
+
+    public LikeComment(Long commentId, Long userId) {
+        this.commentId = commentId;
+        this.userId = userId;
+    }
 
 //    private User user;
 }
