159c0f8aaf4f94481a8c1f67b153915606c429f2
sooni2 <jinong22@gmail.com>
2022-12-17 14:50:23 +0900

    feat - Like Controller, Service 파일 추가

--- CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/controller/LikeController.java
+++ CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/controller/LikeController.java
@@ -2,17 +2,29 @@ package com.hanghae99.catsanddogs.controller;
 
 import com.hanghae99.catsanddogs.dto.CommentResponseDto;
 import com.hanghae99.catsanddogs.dto.ResponseMessage;
+import com.hanghae99.catsanddogs.service.LikeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+@RequiredArgsConstructor
 @RequestMapping("/like")
 @RestController
 public class LikeController {
-    @GetMapping
-    public ResponseEntity<ResponseMessage> commentLike(){
 
+    private final LikeService likeService;
+
+    @PostMapping("/post/{postId}")
+    public ResponseEntity<ResponseMessage> likePost(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        likeService.likePost(postId);
+
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+    @PostMapping("/comment/{commentId}")
+    public ResponseEntity<ResponseMessage> likeComment(@PathVariable Long commentId){
+        likeService.likeComment(commentId)
+        return new ResponseEntity<>(msg, HttpStatus.OK);
     }
 }
---
+++ CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/service/LikeService.java
@@ -0,0 +1,13 @@
+package com.hanghae99.catsanddogs.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class LikeService {
+    @Transactional
+    public void likePost(Long postId) {
+
+
+    }
+}
