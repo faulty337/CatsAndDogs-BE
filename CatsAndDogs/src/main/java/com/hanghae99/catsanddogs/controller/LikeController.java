c86d6723aa4881320d44931be0e4d52dec86da21
sooni2 <jinong22@gmail.com>
2022-12-17 11:08:15 +0900

    try to add sth but need to merge soon

--- CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/controller/LikeController.java
+++ CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/controller/LikeController.java
@@ -1,10 +1,18 @@
 package com.hanghae99.catsanddogs.controller;
 
+import com.hanghae99.catsanddogs.dto.CommentResponseDto;
+import com.hanghae99.catsanddogs.dto.ResponseMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/like")
 @RestController
 public class LikeController {
-
+    @GetMapping
+    public ResponseEntity<ResponseMessage> commentLike(){
+       
+    }
 }
