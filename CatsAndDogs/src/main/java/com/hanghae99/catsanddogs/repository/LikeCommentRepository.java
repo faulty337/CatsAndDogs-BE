d4140c34d24053604363684d8b2aa19e46b7fc46
sooni2 <jinong22@gmail.com>
2022-12-17 20:23:57 +0900

    feat - Like 관련 Repository 수정
    LikePostRepository 추가
    LikeCommentRepository 추가

--- CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/repository/LikeCommentRepository.java
+++ CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/repository/LikeCommentRepository.java
@@ -3,5 +3,8 @@ package com.hanghae99.catsanddogs.repository;
 import com.hanghae99.catsanddogs.entity.LikeComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeCommentRepository extends JpaRepository<LikeComment, Long> {
+    Optional<LikeComment> findByCommentIdAndUserId(Long commentId, Long userId);
 }
--- CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/security/UserDetails/UserDetailsImpl.java
+++ CatsAndDogs/src/main/java/com/hanghae99/catsanddogs/security/UserDetails/UserDetailsImpl.java
@@ -62,4 +62,6 @@ public class UserDetailsImpl implements UserDetails {
     public boolean isEnabled() {
         return false;
     }
+
+
 }
